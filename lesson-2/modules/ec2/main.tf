b66c4444764954fe837befd3be7bab76728bf5a0
Đinh Trọng Nguyên <nguyendt15@vingroup.net>
2023-12-13 09:26:57 +0700

    add apache web

--- lesson-2/modules/ec2/main.tf
+++ lesson-2/modules/ec2/main.tf
@@ -33,6 +33,12 @@ resource "aws_security_group" "nguyendt15_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "nguyendt15_ec2" {
@@ -40,6 +46,14 @@ resource "aws_instance" "nguyendt15_ec2" {
   instance_type = "t2.micro"
   subnet_id = var.subnet_id
   vpc_security_group_ids = [aws_security_group.nguyendt15_security_group.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y httpd
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              echo "<html><body><h1>Hello, World!</h1></body></html>" > /var/www/html/index.html
+              EOF
   tags = {
     Name = "nguyendt15-ec2"
   }
--- lesson-2/modules/vpc/main.tf
+++ lesson-2/modules/vpc/main.tf
@@ -32,7 +32,7 @@ resource "aws_route_table" "nguyendt15_route_table" {
   vpc_id = aws_vpc.nguyendt15_vpc.id
 
   route {
-    cidr_block = "10.1.0.0/16"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.nguyendt15_internet_gateway.id
   }
 
